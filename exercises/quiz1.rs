12edcdb417b3f4487703df72ef66acd02da41f3f
hongde <enginespot@gmail.com>
2023-10-09 21:50:41 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(d:i32) -> i32 {
+    if d > 40 {
+        d
+    } else {
+        d * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
